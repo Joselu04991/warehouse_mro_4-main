445264e0b5c82c3d8f7f4bb5974b45cafb6731eb
Joselu04991 <josesebastiancastillovelasuez@gmail.com>
2025-12-13 23:29:23 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,5 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SECRET_KEY = "clave_super_secreta_mro_2025"
-
-    # Base de datos en archivo local
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'warehouse_mro_v2.db')}"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Carpetas internas
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
-    REPORT_FOLDER = os.path.join(BASE_DIR, "static", "reports")
