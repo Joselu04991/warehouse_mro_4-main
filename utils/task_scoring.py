4f72fba17d27bff812ad478d5018673b12bb1531
Joselu04991 <josesebastiancastillovelasuez@gmail.com>
2025-12-14 22:42:18 -0500

    Update task_scoring.py

--- utils/task_scoring.py
+++ utils/task_scoring.py
@@ -3,21 +3,15 @@ from models import db
 from models.user import User
 
 def aplicar_puntaje(task):
-    usuario = User.query.get(task.assigned_to_id)
-
-    if not usuario:
+    user = User.query.get(task.assigned_to_id)
+    if not user:
         return
 
     dias_diferencia = (task.fecha_completado - task.fecha_limite).days
 
     if dias_diferencia <= 0:
-        usuario.score += 1
+        user.score = min(user.score + 1, 20)
     else:
-        usuario.score -= 1
+        user.score = max(user.score - 1, 0)
 
-    if usuario.score < 0:
-        usuario.score = 0
-    if usuario.score > 20:
-        usuario.score = 20
-
-    db.session.add(usuario)
+    db.session.add(user)
