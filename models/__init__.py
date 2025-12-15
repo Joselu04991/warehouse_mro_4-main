f5d3b2de616140e1a923c516f07dca4b92604c65
Joselu04991 <josesebastiancastillovelasuez@gmail.com>
2025-12-14 20:29:43 -0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -21,4 +21,5 @@ from .inventory_history import InventoryHistory
 from .warehouse2d import WarehouseLocation
 from .actividad import ActividadUsuario
 from .inventory_count import InventoryCount
-
+from .task import Task
+from .user_score import UserScore
