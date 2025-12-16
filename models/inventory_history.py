df5634c41fcea13dccffa0c86f94a79733b9a07f
Joselu04991 <josesebastiancastillovelasuez@gmail.com>
2025-12-16 13:34:19 -0500

    Update inventory_history.py

--- models/inventory_history.py
+++ models/inventory_history.py
@@ -6,21 +6,26 @@ class InventoryHistory(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
+    user_id = db.Column(db.Integer, nullable=False, index=True)
+
     snapshot_id = db.Column(db.String(64), nullable=False, index=True)
     snapshot_name = db.Column(db.String(150), nullable=False)
 
-    material_code = db.Column(db.String(50), nullable=False)
+    material_code = db.Column(db.String(50), nullable=False, index=True)
     material_text = db.Column(db.String(255), nullable=False)
     base_unit = db.Column(db.String(20), nullable=False)
-    location = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(50), nullable=False, index=True)
 
     libre_utilizacion = db.Column(db.Float, default=0)
 
-    closed_by = db.Column(db.String(120))      # usuario
-    closed_at = db.Column(db.DateTime)          # hora PERU
-
     creado_en = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return f"<History {self.snapshot_name}>"
+    # extras
+    source_type = db.Column(db.String(30), default="DIARIO")     # DIARIO / HISTORICO / CIERRE
+    source_filename = db.Column(db.String(255), nullable=True)
 
+    closed_by = db.Column(db.String(120), nullable=True)
+    closed_at = db.Column(db.DateTime, nullable=True)
+
+    def __repr__(self):
+        return f"<History {self.snapshot_name} - {self.material_code}>"
